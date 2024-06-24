a20a539717713acf1b97d2d8412a929c93a93060
Oleksii Zhyvotivskyi <gamesdashev@gmail.com>
2024-06-24 12:56:43 +0200

    Add build files to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './slot-machine-project/frontend/src/index.js',
   output: {
     path: path.resolve(__dirname, 'slot-machine-project/frontend/public'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'slot-machine-project/frontend/public'),
+      directory: path.join(__dirname, 'slot-machine-project/frontend/public'),
     },
     compress: true,
-    port: 8087,
-    open: true,
+    port: 8080,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './slot-machine-project/frontend/public/index.html',
-    }),
-  ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: 'asset/resource',
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
-    ],
-  },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
 };
